3c2165663a083d6a3b465e310f8e3c7634181a15
JJLim <lim.jess9@gmail.com>
2015-06-08 00:30:58 -0400

    Update review controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,32 @@
 class ReviewsController < ApplicationController
+  before_filter :load_camp
+
   def show
+    @review = Review.find(params[:id])
   end
 
   def create
+    @review = @camp.reviews.biuld(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to camps_path, notice: 'Review created successfully'
+    else
+      render 'products/show'
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def review_params
+    params.review(:review).permit(:comment, :camp_id)
+  end
+
+  def load_camp
+    @camp = Camp.find(params[:camp_id])
   end
 end
