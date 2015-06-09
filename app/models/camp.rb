6fe62086beb98dd7b16ebb65a09c9147a40786bc
JJLim <lim.jess9@gmail.com>
2015-06-09 18:56:35 -0400

    Dirty tracking camp address changes to geocode

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -6,6 +6,7 @@ class Camp < ActiveRecord::Base
   has_many :users, through: :reviews
 
   geocoded_by :full_address
+  after_validation :geocode, if: (:address_changed? || :city_changed? || :province_changed?)
 
   def full_address
     "#{address} #{city} #{province}"
