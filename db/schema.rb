0d9a5538c5cc234bcc25ada6276c717845c9a113
JJLim <lim.jess9@gmail.com>
2015-06-08 22:14:29 -0400

    Add bio attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608035512) do
+ActiveRecord::Schema.define(version: 20150609020910) do
 
   create_table "camps", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150608035512) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
