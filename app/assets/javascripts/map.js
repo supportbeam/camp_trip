07731f4e27c9d56440e901b64e8e4126bd9fed9f
JJLim <lim.jess9@gmail.com>
2015-06-10 01:28:02 -0400

    Add google map markers to camp location and nearbys

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,24 +13,46 @@ Map.prototype.init = function(latitude, longitude) {
   this.canvas = new google.maps.Map(this.mapId, options);
 }
 
-Map.prototype.addMarker = function(latitude, longitude) {
+Map.prototype.addMarker = function(latitude, longitude, markerImage) {
   var options = {
     position: {lat: latitude, lng: longitude},
-    map: this.canvas
+    map: this.canvas,
+  }
+
+  if (markerImage) {
+    options.icon = markerImage;
   }
 
   var myMarker = new google.maps.Marker(options);
   this.markers.push(myMarker);
 }
 
+Map.prototype.removeMarker = function(marker) {
+  // remove marker from map
+  marker.setMap(null);
+
+  var index = this.markers.indexOf(marker);
+  this.markers.splice(index, 1);
+}
+  
+Map.prototype.findMarker = function(latitude, longitude) {
+  return marker;
+}
 
 $(document).on('page:load ready', function() {
   if ($('#map-canvas').length) {
-    var latitude = window.latitude;
-    var longitude = window.longitude;
+    var latitude = $('#map-canvas').data('latitude');
+    var longitude = $('#map-canvas').data('longitude');
+    var customIconImage = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
 
     var myMap = new Map($('#map-canvas')[0]);
     myMap.init(latitude, longitude);
     myMap.addMarker(latitude, longitude);
+
+    if (window.nearbys) {
+      nearbys.forEach(function(coord) {
+        myMap.addMarker(parseFloat(coord.latitude), parseFloat(coord.longitude), customIconImage);
+      });
+    }
   }
 });
